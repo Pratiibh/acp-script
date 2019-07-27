dc5ebdb14a2bde9ad01f0fd99d107ce1bec76016
leeroywking <leeroywking@gmail.com>
2019-07-27 12:48:13 -0700

    skeletoned out the shell command

--- README.md
+++ README.md
@@ -10,11 +10,10 @@
 ## Expected parameters
   * input should look like 
     ``` 
-    acp -m "This is the commit message"
+    acp "This is the commit message"
     ```
 
 ## Assumptions
   * You are in the branch you want to commit to and in the PWD of your repo
   * you are at the base level of your repo 
     * we are going to use ``` add . ``` in this command so you must be in the root of your repo
-    
---
+++ acp.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+# Bash is a series of command line calls so you can use it to do things like this
+# echo "This is the output from your bash script"
+
+# Or things like this 
+# ls
+# ls -al
+
+# really anything that you would normally do on the command line 
+# git status
+# cd ~
+# cd -
+
+# Ultimately we want this script to execute three commands\
+echo '
+git add .
+git commit -m "with variable text"
+git push origin branch-name
+' > /dev/null
+
+echo '
+If we are going to imagine this like its javascript we would think of this as a function with syntax something like 
+' > /dev/null
+
+# This below syntax is a special kind of bash command which lets me send the output of something to 'null' which just dumps it (I used it because its prettier than most comments)
+echo '
+Psuedo Code
+function acp(text,branch){
+  git add .;
+  git commit -m `${text}`;
+  git push origin ${branch};
+}
+' > /dev/null
